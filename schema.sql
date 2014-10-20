8acf40064f4745e461eb47f32f1b6cbb9e619a20
Sam Marder <sam.eclipse@marder.co>
2014-10-20 16:58:21 -0400

    Added some important files from the original project.
    Includes a schema file for quickly getting the database up and running.
    Includes a sample tweet to get a feel for what tweets look like (from the Streaming API).
    Includes the PowerPoint presentation I gave when I presented this project. It includes detailed notes about what I said.

---
+++ schema.sql
@@ -0,0 +1,21 @@
+/*
+ * Running every command in this file will completely set up the MySQL database.
+ *
+ * Specifically, run the following command from terminal:
+ *
+ *     mysql -u root -p < schema.sql
+ */
+
+CREATE DATABASE IF NOT EXISTS twitter_db;
+
+GRANT ALL PRIVILEGES ON twitter_db.* TO 'twitter'@'localhost' IDENTIFIED BY 'tweet_tweet';
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS twitter_db.tweets_db (
+        id BIGINT(20) unsigned NOT NULL AUTO_INCREMENT,
+        text varchar(160) NOT NULL,
+        hash_tags varchar(160) NOT NULL,
+        created_at BIGINT(20) NOT NULL,
+        location varchar(20) DEFAULT NULL,
+        PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
